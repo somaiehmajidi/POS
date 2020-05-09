20a24632a831d12bbf5d99236ade298331ad9d1d
somaiehmajidi <somaieh.majidi@gmail.com>
2020-05-09 17:06:07 +0430

    added selected item to cart

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Order } from '../interfaces/Order'
+import { Order } from '../interfaces/Order';
 import { DataService } from '../services/data.service';
 
 @Component({
@@ -11,22 +11,27 @@ export class CartComponent implements OnInit {
 
   orders: Order[] = [];
   total: number = 0;
+  selectedItem: any;
+  
   constructor(private dataService: DataService) { }
 
   ngOnInit(): void {
    this.dataService.getOrders()
    .subscribe(orders =>{
       this.orders = orders;
-      
    });
-//if (this.orders.length > 0 ){
-//}
+   //this.selectedItem = this.orders.slice(-1)[0];
+   
   }
 
   ngAfterContentChecked(){
     this.total = this.orders.reduce(function(prev, cur){
        return prev+cur.totalPrice;
       }, 0);
+  }
+
+  onSelect(order: Order): void{
+    this.selectedItem = order;
   }
 
 }
