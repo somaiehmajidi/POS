912c0ed8e92cb055194c2f44ab4d81df1f0bd036
somaiehmajidi <somaieh.majidi@gmail.com>
2020-05-10 19:45:55 +0430

    added css to set icons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -15,7 +14,7 @@ import { FilterPipe } from './filter.pipe';
     HeaderComponent,
     ProductsComponent,
     CartComponent,
-    FilterPipe
+    FilterPipe,
   ],
   imports: [
     BrowserModule,
