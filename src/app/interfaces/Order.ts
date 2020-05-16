af7739d3b19032d68241981fb06c980ee5ab59f4
somaiehmajidi <somaieh.majidi@gmail.com>
2020-05-16 04:55:19 +0430

    deleted old model types

--- src/app/interfaces/Order.ts
+++
@@ -1,6 +0,0 @@
-export interface Order{
-   name: string, 
-   price: number, 
-   totalPrice: number, 
-   unit: number 
-}
--- src/app/interfaces/Product.ts
+++
@@ -1,9 +0,0 @@
-export interface Product{
-    id: number,
-    name: string,
-    price: number,
-    units: number,
-    imageUrl: string,
-    category: string
-}
-
