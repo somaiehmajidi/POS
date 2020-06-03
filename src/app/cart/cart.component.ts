e2ab175654bd01d7f3ae15bc22bc5dd826b4478d
somaiehmajidi <somaieh.majidi@gmail.com>
2020-06-04 00:25:46 +0430

    added payment component

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -11,12 +11,9 @@ import { CustomerService } from '../shared/customer.service';
 })
 export class CartComponent implements OnInit {
 
-  //date:Date = new Date();
-
   orders: Order[] = [];
   invoices: Invoice[] = []
   total: number = 0;
-  selectedItem: Order;
   assignedCustomer: Customer;
 
   selectedInvoice;
@@ -31,23 +28,19 @@ export class CartComponent implements OnInit {
     this.selectedInvoice = this.cartService.selectedInvoice;
     this.selectedOrder = this.cartService.selectedOrder;
     
-    //this.orders = this.cartService.selectedInvoice.orders;
     if (this.customerService.selectedCustomer){
       this.assignedCustomer = this.customerService.selectedCustomer;
     }
   }
 
   onSelect(order: Order): void{
-    //this.selectedItem = order;
     this.cartService.onSelect(order);
-    //this.selectedItem = this.cartService.selectedItem;
   }
 
   addCustomer(){
     this.router.navigate(['/customers']);
   }
 
-  //this.selectedItem = this.orders.slice(-1)[0];
   
   ngAfterContentChecked(){
     // this.total = this.orders.reduce(function(prev, cur){
